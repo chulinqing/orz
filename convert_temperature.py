8a6b0ed1dcafb0a9c2ca7a5ad3a50dca9d7e5294
unknown <clqdersieg@gmail.com>
2023-05-25 13:38:57 +0200

    meaningful names render readability

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,4 +1,4 @@
-def fahrenheit_to_celsius(f):
-         c = (f - 32) * (5.0/9.0)
+def fahrenheit_to_celsius(temp_fahrenheit):
+         temp_fahrenheit = (temp_fahrenheit - 32) * (5.0/9.0)
          return c
 
