00f6f12a667a6540268c305ae74d4d0033f8d02c
unknown <clqdersieg@gmail.com>
2023-05-25 14:23:15 +0200

    souce

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,4 +1,11 @@
 def fahrenheit_to_celsius(temp_fahrenheit):
+         """
+	convers temperature in fahrenheit to celsius
+	
+	parameters:
+	temperature_fahrenheir
+	"""
+
          temp_fahrenheit = (temp_fahrenheit - 32) * (5.0/9.0)
          return c
 
